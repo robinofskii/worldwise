220e293d5a27f6de75fb6e3943d6b213f370733d
robinofskii <rvandenberg@yieldcomputer.com>
2023-09-25 22:01:08 +0200

    feat: :lipstick: style the page nav

--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -1,17 +1,29 @@
 import { NavLink } from 'react-router-dom';
 
+import styles from './PageNav.module.scss';
+
 const PageNav = () => {
+  const checkActive = ({ isActive }: { isActive: boolean }) => {
+    return isActive ? styles.active : '';
+  };
+
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" className={checkActive}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink to="/pricing" className={checkActive}>
+            Pricing
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/product">Product</NavLink>
+          <NavLink to="/product" className={checkActive}>
+            Product
+          </NavLink>
         </li>
       </ul>
     </nav>
